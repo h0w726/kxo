161e3a603f63ea39fc4c1d1c13a1d5d74b4089ee
¨h0w726¨ <how77766@gmail.com>
2025-05-03 20:42:08 +0800

    Move board rendering from kernel to user space
    This commit moves the board rendering logic from kernel space to user
    space.Previously, the kernel sent the entire board state to user space
    for display.Now, it transmits only the positions of placed pieces,
    reducing the amount of data transferred.
    
    This simplifies the kernel module, reduces memory barriers,
    and improves overall performance by minimizing data transmission.

--- xo-user.c
+++ xo-user.c
@@ -14,6 +14,8 @@
 #define XO_DEVICE_FILE "/dev/kxo"
 #define XO_DEVICE_ATTR_FILE "/sys/class/kxo/kxo/kxo_state"
 
+static char draw_buffer[DRAWBUFFER_SIZE];
+
 static bool status_check(void)
 {
     FILE *fp = fopen(XO_STATUS_FILE, "r");
@@ -82,6 +84,25 @@ static void listen_keyboard_handler(void)
     close(attr_fd);
 }
 
+/* Draw the board into draw_buffer */
+static int draw_board(char *table)
+{
+    int i = 0, k = 0;
+    draw_buffer[i++] = '\n';
+    draw_buffer[i++] = '\n';
+    while (i < DRAWBUFFER_SIZE) {
+        for (int j = 0; j < (BOARD_SIZE << 1) - 1 && k < N_GRIDS; j++) {
+            draw_buffer[i++] = j & 1 ? '|' : table[k++];
+        }
+        draw_buffer[i++] = '\n';
+        for (int j = 0; j < (BOARD_SIZE << 1) - 1; j++) {
+            draw_buffer[i++] = '-';
+        }
+        draw_buffer[i++] = '\n';
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     if (!status_check())
@@ -91,7 +112,7 @@ int main(int argc, char *argv[])
     int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 
-    char display_buf[DRAWBUFFER_SIZE];
+    char display_buf[N_GRIDS];
 
     fd_set readset;
     int device_fd = open(XO_DEVICE_FILE, O_RDONLY);
@@ -116,9 +137,9 @@ int main(int argc, char *argv[])
         } else if (read_attr && FD_ISSET(device_fd, &readset)) {
             FD_CLR(device_fd, &readset);
             printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
-            read(device_fd, display_buf, DRAWBUFFER_SIZE);
-            display_buf[DRAWBUFFER_SIZE - 1] = '\0';
-            printf("%s", display_buf);
+            read(device_fd, display_buf, N_GRIDS);
+            draw_board(display_buf);
+            printf("%s", draw_buffer);
         }
     }
 
